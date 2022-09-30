4555e88b0f3e5303c595d31241b4bd024d65d2af
Joshua Miertschin <the9rtyt@gmail.com>
2022-09-29 22:53:06 -0500

    moved animations to their own files

--- lib/rgbpi/guards.ex
+++ lib/rgbpi/guards.ex
@@ -2,6 +2,9 @@ defmodule RGBPi.Guards do
   defguard is_strip(s)
            when s in 0..1
 
+  defguard is_hue(h)
+           when h in 0..255
+
   defguard is_rgb(r, g, b)
            when r in 0..255 and g in 0..255 and b in 0..255
 
