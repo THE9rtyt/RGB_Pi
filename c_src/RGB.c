f8a1d5a42144822e8e95e8057e85be8b5f43069d
Joshua Miertschin <the9rtyt@gmail.com>
2022-09-29 22:41:29 -0500

    move hsvrgb to header

--- c_src/RGB.c
+++ c_src/RGB.c
@@ -16,7 +16,7 @@
 
 #include "rpi_ws281x/ws2811.h"
 #include "port_interface.h"
-#include "RGB_types.h"
+#include "hsv.h"
 
 void set_pixel(ws2811_channel_t *channels) {
   uint8_t strip;
@@ -49,71 +49,6 @@ void fill_strip(ws2811_channel_t *channels) {
     channels[strip].leds[pixel] = color;
   };
   reply_ok();
-}
-
-#define THIRD 0x56
-
-ws2811_led_t hsv_to_rgb(HSV hsv) {
-  uint8_t hue = ((hsv >> 16) & 0xFF);
-  uint8_t sat = ((hsv >> 8) & 0xFF);
-  uint8_t val = ((hsv) & 0xFF);
-
-
-  uint8_t invsat = 255 - sat;
-  uint8_t brightness_floor = (val * invsat) / 255;
-
-  uint8_t color_amplitude = val - brightness_floor;
-
-  uint8_t section = hue / THIRD;//0..2
-
-  uint8_t offset;
-  //bug fix since I can't figure it out
-  if (hue == 255) {
-    offset = THIRD-1;
-  } else {
-    offset = hue % THIRD;
-  }
-  
-  uint8_t rampup = offset;
-  uint8_t rampdown = (THIRD - 1) - offset;
-
-  // compute color-amplitude-scaled-down versions of rampup and rampdown
-  uint8_t rampup_amp_adj   = (rampup   * color_amplitude) / (256 / 3);
-  uint8_t rampdown_amp_adj = (rampdown * color_amplitude) / (256 / 3);
-
-  // add brightness_floor offset to everything
-  uint8_t rampup_adj_with_floor   = rampup_amp_adj   + brightness_floor;
-  uint8_t rampdown_adj_with_floor = rampdown_amp_adj + brightness_floor;
-
-  if( section ) {
-    if( section == 1) {
-      // section 1: 0x56..0xAA
-      return((brightness_floor << 16) |
-      (rampdown_adj_with_floor << 8) |
-      (rampup_adj_with_floor))& 0xFFFFFFFF;
-    } else {
-      // section 2; 0xAB..0xFF
-      return((rampup_adj_with_floor << 16) |
-      (brightness_floor << 8) |
-      (rampdown_adj_with_floor)) & 0xFFFFFFFF;
-    }
-  } else {
-    // section 0: 0x00..0x55
-    return((rampdown_adj_with_floor << 16) |
-    (rampup_adj_with_floor << 8) |
-    (brightness_floor)) & 0xFFFFFFFF;
-  }
-}
-
-void hsvrgb() {
-  ws2811_led_t color;
-  char nl;
-  if (scanf("%x%c", &color, &nl) != 2 || nl != '\n') {
-    reply_error("Argument error");
-    return;
-  };
-
-  reply_ok_payload("%x", hsv_to_rgb(color));
 }
 
 void fill_rainbow(ws2811_channel_t *channels) {
@@ -153,6 +88,17 @@ void fill_hue(ws2811_channel_t *channels) {
   };
 
   reply_ok();
+}
+
+void hsvrgb() {
+  ws2811_led_t color;
+  char nl;
+  if (scanf("%x%c", &color, &nl) != 2 || nl != '\n') {
+    reply_error("Argument error");
+    return;
+  };
+
+  reply_ok_payload("%x", hsv_to_rgb(color));
 }
 
 /*
